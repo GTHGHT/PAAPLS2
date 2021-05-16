d2b65d98627b44da89535e0b6798f32db5229848
GTHGHT <gilang.example@gmail.com>
2021-05-16 21:38:21 +0800

    penentuan mekanisme penyimpanan manajemen_barang.py

--- manajemen_barang.py
+++ manajemen_barang.py
@@ -1,3 +1,19 @@
+import numpy as np
+import pandas as pd
+
 class ManajemenBarang:
-    # Disini tempat kodingan manajemen barang
-    pass
+    def __init__(self):
+        csv_path = "data/barang.csv"
+        try:
+            self.data = pd.read_csv(csv_path)
+        except FileNotFoundError:
+            with open(csv_path, "w") as barang:
+                barang.write("Barcode,Nama Barang,Stok")
+            self.data = pd.read_csv(csv_path)
+
+    def print_data(self):
+        print(self.data)
+
+
+if __name__ == '__main__':
+    ManajemenBarang().print_data()
