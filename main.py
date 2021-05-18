8f5184f197659c66ab33301dace858388db01c9e
GTHGHT <gilang.example@gmail.com>
2021-05-18 17:33:13 +0800

    penentuan mekanisme penyimpanan manajemen_barang.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
+from manajemen_barang import ManajemenBarang
+from pencatatan_pesanan import PencatatanPesanan
 
-
-global manajemen_barang
-global pencatatan_pesanan
+manage_barang = ManajemenBarang()
+catat_pesanan = PencatatanPesanan()
 
 
 def menu_pesanan():
+    global catat_pesanan
+    catat_pesanan.print_pesanan()
     print("Menu Pencatatan Pesanan")
     print("1. Tambah Pesanan")
     print("2. Edit Pesanan")
@@ -13,6 +16,23 @@ def menu_pesanan():
     print("4. Terima Pesanan")
     print("5. Exit")
     menu_pesanan_in = input("Pilih Aksi : ")
+    if menu_pesanan_in == "1":
+        jenis_barang = int(input(f"{'Masukkan Jumlah Jenis Barang': <30} = "))
+        if jenis_barang > 0:
+            daftar_barang = list()
+            for i in range(jenis_barang):
+                print()
+                print(f"Barang ke-{i + 1}")
+                print(f"{'':-<50}")
+                barcode = input(f"{'Masukkan Barcode': <30} = ")
+                nama = input(f"{'Masukkan Nama Barang': <30} = ")
+                jumlah = int(input(f"{'Masukkan Jumlah Barang': <30} = "))
+                daftar_barang.append([barcode, nama, jumlah, ""])
+            po_id = catat_pesanan.add_pesanan(daftar_barang)
+            print("\nPenambahan Preorder Berhasil")
+            print(f"ID Preorder = {po_id}")
+        else:
+            print("\nPenambahan Preorder Gagal")
 
 
 def menu_barang():
@@ -24,8 +44,6 @@ def menu_barang():
 
 
 if __name__ == '__main__':
-    manage_barang = manajemen_barang()
-    catat_pesanan = pencatatan_pesanan()
     while True:
         print("Aplikasi Manajemen Gudang Supermarket")
         print("1. Pencatatan Pesanan")
--- pencatatan_pesanan.py
+++ pencatatan_pesanan.py
@@ -1,9 +1,14 @@
+import datetime
+
 import numpy as np
 import pandas as pd
+
 
 class PencatatanPesanan:
 
     def __init__(self):
+        self.PESANAN_COLUMN = ["ID Pesanan", "Tanggal Pesan", "Tanggal Terima", "Status"]
+        self.BARANG_PESANAN_COLUMN = ["Barcode", "Nama Barang", "Jumlah Barang", "Expired"]
         csv_path = "data/pesanan.csv"
         try:
             self.pesanan_df = pd.read_csv(csv_path)
@@ -12,10 +17,43 @@ class PencatatanPesanan:
                 pesanan_csv.write("ID Pesanan,Tanggal Pesan,Tanggal Terima,Status\n")
             self.pesanan_df = pd.read_csv(csv_path)
 
+        self.char_increment = "A"
+        self.num_increment = 0
+
         try:
             df_by_id = self.pesanan_df.groupby("ID Pesanan")
-            barang_pesanan_list = (pd.read_csv(f"data/{id_}.csv" for id_, _ in df_by_id))
-            self.barang_pesanan_df = pd.concat(barang_pesanan_list, ignore_index=True)
+            if len(self.pesanan_df)>0:
+                barang_pesanan_list = list()
+                for id_, _ in df_by_id:
+                    barang_pesanan_list.append(pd.read_csv(f"data/pesanan/{id_}.csv"))
+                    if id_[0] > self.char_increment:
+                        self.char_increment = id_[0]
+                    if int(id_[1:]) > self.num_increment:
+                        self.num_increment = int(id_[1:])
+                self.barang_pesanan_df = pd.concat(barang_pesanan_list, ignore_index=True)
+            else:
+                self.barang_pesanan_df = pd.DataFrame([], columns=self.BARANG_PESANAN_COLUMN)
         except FileNotFoundError:
             raise FileNotFoundError
+
+    def get_id(self):
+        self.num_increment += 1
+        if self.num_increment == 10000:
+            self.char_increment = chr(ord(self.char_increment) + 1)
+            self.num_increment = 1
+        return f"{self.char_increment}{self.num_increment:0>4}"
+
+    def add_pesanan(self, list_barang):
+        barang_df = pd.DataFrame(list_barang, columns=self.BARANG_PESANAN_COLUMN)
+        id_pesanan = self.get_id()
+        barang_df.to_csv(f"data/pesanan/{id_pesanan}.csv")
+        new_pesanan_df = pd.DataFrame([[id_pesanan, str(datetime.date.today()), "", "Belum Diterima"]],columns=self.PESANAN_COLUMN)
+        self.pesanan_df.append(new_pesanan_df, ignore_index=True)
+        self.pesanan_df.to_csv("data/pesanan.csv")
+        return id_pesanan
+
+    def print_pesanan(self):
+        print(self.pesanan_df)
+        print(self.barang_pesanan_df)
+
 
